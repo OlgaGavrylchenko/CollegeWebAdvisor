b79fb4e43dedbe64471c98845cd1c5b14f99e0d2
OlgaGavrylchenko <15853578+OlgaGavrylchenko@users.noreply.github.com>
2018-01-17 16:16:44 -0500

    Add files via upload

---
+++ schedule.sql
@@ -0,0 +1,61 @@
+INSERT INTO class_schedule (course_id, course_name, course_credits, meet_time, meet_date,
+meet_location, course_type) VALUES ('OIM 220', 'Keyboarding I', 3, '5:00 - 6:15', 'Tu', 'B101', 'CR');
+
+INSERT INTO class_schedule (course_id, course_name, course_credits, meet_time, meet_date,
+meet_location, course_type) VALUES ('ENG 111', 'English I', 3, '8:30 - 11:45', 'W', 'C202', 'CR');
+
+INSERT INTO class_schedule (course_id, course_name, course_credits, meet_time, meet_date,
+meet_location, course_type) VALUES ('CMP 545', 'Web Programming', 3, '6:30 - 9:15' ,'F', 'D117', 'CR');
+
+INSERT INTO class_schedule (course_id, course_name, course_credits, meet_time, meet_date,
+meet_location, course_type) VALUES ('CMP 237', 'C++ Programming', 3, '6:30 - 9:15', 'Th', 'D121', 'CR');
+
+INSERT INTO class_schedule (course_id, course_name, course_credits, meet_time, meet_date,
+meet_location, course_type) VALUES ('CIT 239' 'Java Programming', 3, '5:45 - 7:45', 'Tu, Th', 'D119', 'CR');
+
+INSERT INTO class_schedule (course_id, course_name, course_credits, meet_time, meet_date,
+meet_location, course_type) VALUES ('ENG 112', 'College English II', 3, '12:00 - 2:45', 'Su', 'B212', 'CR');
+
+INSERT INTO class_schedule (course_id, course_name, course_credits, meet_time, meet_date,
+meet_location, course_type) VALUES ('ACC 204', 'Business Income Tax', 3, '9:00 - 11:45', 'M, W', 'B222', 'CR');
+
+INSERT INTO class_schedule (course_id, course_name, course_credits, meet_time, meet_date,
+meet_location, course_type) VALUES ('MAT 281', 'Calculus I', 3, '3:15 - 5:15', 'W', 'B138', 'CR');
+
+INSERT INTO class_schedule (course_id, course_name, course_credits, meet_time, meet_date,
+meet_location, course_type) VALUES ('BIO 203', 'Anatomy/Phys I/Lab', 3, '12:00 - 2:45', 'Tu, Th', 'D310', 'CR');
+
+INSERT INTO class_schedule (course_id, course_name, course_credits, meet_time, meet_date,
+meet_location, course_type) VALUES ('PHY 251', 'College Physics I', 3, '9:00 - 10:45', 'M, W', 'D232', 'CR');
+
+INSERT INTO class_schedule (course_id, course_name, course_credits, meet_time, meet_date,
+meet_location, course_type) VALUES ('NC100', 'Basic Cookie Baking', 0, '6:00 - 8:00', 'M', 'E415', 'NC');
+
+INSERT INTO class_schedule (course_id, course_name, course_credits, meet_time, meet_date,
+meet_location, course_type) VALUES ('NC200', 'Advanced Tire Inflation', 0, '6:00 - 8:00', 'W', 'D100', 'NC');
+
+INSERT INTO class_schedule (course_id, course_name, course_credits, meet_time, meet_date,
+meet_location, course_type) VALUES ('NC300', 'Intro to Sinusitis', 0, '6:00 - 8:00', 'W', 'B345', 'NC');
+
+INSERT INTO class_schedule (course_id, course_name, course_credits, meet_time, meet_date,
+meet_location, course_type) VALUES ('NC400', 'Shoe Polish and You', 0, '6:15 - 8:15', 'Tu', 'C202', 'NC');
+
+INSERT INTO class_schedule (course_id, course_name, course_credits, meet_time, meet_date,
+meet_location, course_type) VALUES ('APU-554', 'You and Your Community', 0, '5:15 - 7:15', 'Tu, F', 'C222', 'NC');
+
+INSERT INTO class_schedule (course_id, course_name, course_credits, meet_time, meet_date,
+meet_location, course_type) VALUES ('APU-555', 'The Community & USA', 0, '2:45 - 4:15', 'M, F', 'B222', 'NC');
+
+INSERT INTO class_schedule (course_id, course_name, course_credits, meet_time, meet_date,
+meet_location, course_type) VALUES ('HSE-100', 'High School Equivalency', 0, '12:45 - 2:15', 'W', 'A102', 'NC');
+
+INSERT INTO class_schedule (course_id, course_name, course_credits, meet_time, meet_date,
+meet_location, course_type) VALUES ('SSL-101', 'Spk/Listen & Read/Writ Lvl I', 0, '12:45 - 2:15', 'W, F', 'A152', 'NC');
+
+INSERT INTO class_schedule (course_id, course_name, course_credits, meet_time, meet_date,
+meet_location, course_type) VALUES ('SSL-102', 'Spk/Listen & Read/Writ Lvl II', 0, '12:45 - 2:15', 'M, Th', 'A152', 'NC');
+
+INSERT INTO class_schedule (course_id, course_name, course_credits, meet_time, meet_date,
+meet_location, course_type) VALUES ('VMA-102', 'Composition & Color', 3, '12:45 - 2:15', 'F, Su', 'B119', 'CR');
+
+
